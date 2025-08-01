75efc52991c220d4f6b90938d6933a08d26a0c1c
ibrahims65 <ibrahims65@gmail.com>
2025-08-01 13:07:16 -0400

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,9 @@
-const prisma = require('../lib/prisma');
+// middleware/auth.js
 
-module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-        if (req.session.user && req.session.user.id) {
-            return next();
-        }
-        req.flash('error', 'Please log in first');
-        res.redirect('/login');
-    },
-    ensureAdmin: async function(req, res, next) {
-        if (req.session.userId) {
-            const user = await prisma.user.findUnique({ where: { id: req.session.userId }, include: { role: true } });
-            if (user && user.role.name === 'Admin') {
-                return next();
-            }
-        }
-        res.redirect('/login');
-    },
-    checkRole: function(roles) {
-        return async (req, res, next) => {
-            if (req.session.userId) {
-                const user = await prisma.user.findUnique({ where: { id: req.session.userId }, include: { role: true } });
-                if (user && roles.includes(user.role.name)) {
-                    return next();
-                }
-            }
-            res.status(403).send('Forbidden');
-        };
-    }
+exports.ensureAuthenticated = (req, res, next) => {
+  if (req.session.user) {
+    return next();
+  }
+  req.flash('error', 'Please log in first');
+  res.redirect('/login');
 };
