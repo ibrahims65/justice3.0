5d95240885f09abcc249beba0cb947c9589bb9ac
ibrahims65 <ibrahims65@gmail.com>
2025-08-01 13:06:50 -0400

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,43 @@
 // controllers/authController.js
-const { authenticate } = require('../src/services/authProvider');
+const ldap = require('../src/ldap/ldap-db');
 
 exports.getLogin = (req, res) => {
-  res.render('login', { error: req.flash('error') });
+  res.render('login', {
+    pageTitle: 'Login'
+  });
 };
 
-exports.postLogin = async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await authenticate(username, password);
-    req.session.user = user; // or generate JWT
-    res.redirect('/police');
-  } catch (e) {
-    req.flash('error', 'Invalid credentials');
-    res.redirect('/login');
-  }
-};
+exports.postLogin = (req, res, next) => {
+  const { uid, password } = req.body;
+  const dn = `uid=${uid},ou=users,dc=justice,dc=local`;
 
-exports.getLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  ldap.getEntry(dn, (err, entry) => {
     if (err) return next(err);
+
+    // no user or bad password
+    if (!entry || !ldap.verifyPassword(entry, password)) {
+      req.flash('error', 'Invalid credentials');
+      return res.redirect('/login');
+    }
+
+    // store minimal info in session
+    req.session.user = {
+      uid:      entry.attributes.uid,
+      cn:       entry.attributes.cn,
+      memberof: entry.attributes.memberof
+    };
+
+    // force save then redirect by group
+    req.session.save(saveErr => {
+      if (saveErr) return next(saveErr);
+      const group = req.session.user.memberof.split('=')[1].toLowerCase();
+      res.redirect(`/${group}`);
+    });
+  });
+};
+
+exports.getLogout = (req, res) => {
+  req.session.destroy(() => {
     res.redirect('/login');
   });
 };
